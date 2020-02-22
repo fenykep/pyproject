8a2fcffc11356bf349040ff12522098c13b759d5
fenykep <abel.fenykep@gmail.com>
2020-02-22 10:55:32 +0100

    Removed most of the insulting promt messages

--- finalProgram.py
+++ finalProgram.py
@@ -5,7 +5,7 @@ c = conn.cursor()
 #Superimportant!! Somehow you have to either show the rowID or every time you remove a row, you have to update all the rowidS
 def newEntry(a):
 	c.execute("INSERT INTO students(name, address, class, matg, scig, eng, dug, artg) VALUES (?,?,?,?,?,?,?,?)",push)
-	conn.commit() #tuggyukmi
+	conn.commit() 
 
 def delete(x):
 	c.execute("DELETE FROM students WHERE rowid=?",str(x))
@@ -13,7 +13,6 @@ def delete(x):
 
 def editkegy(i,valtoc,val):
 	val="'"+val+"'"
-	#print('UPDATE students SET %s=%s WHERE rowid=%s' %(valtoc,val,i))
 	c.execute('UPDATE students SET %s=%s WHERE rowid=%s' %(valtoc,val,i))
 	conn.commit()
 
@@ -24,7 +23,8 @@ def editke(stuff):
 while True:
 	sw=raw_input("[A]dd, [D]elete, [E]dit, [S]earch, [P]aint")
 	if (sw=="A" or sw=="a"):
-		print("Jovan bazmeg majd hozzaadom, pattogja ma anyadba el!")
+		print("Please enter the details of the new student.")
+		#This could be cleaned up a bit
 		push=()
 		push+=(raw_input("Name: "),)
 		push+=(raw_input("Address: "),)
@@ -36,19 +36,18 @@ while True:
 		push+=(raw_input("Art grade: "),)
 		newEntry(push)
 	elif (sw=="D" or sw=="d"):
-		print("Torold a segged, ne engem baszogass.")
-		delete(raw_input("Mindegyis, mit akarsz?"))
+		delete(raw_input("Please enter the ID of the row you want to delete."))
 	elif (sw=="E" or sw=="e"):
-		print("Nem kolaztad volna el a tamogatast, most lenne szerkesztoseg.")
-		sortoedit=int(raw_input("Which line shall I do?"))
+		print("It's okay, we all make mistakes sometimes.")
+		sortoedit=int(raw_input("Which row would you like to edit?"))
 		lifechoices=raw_input("Do you want to edit a specific [V]alue or a whole [R]ow?")
 		if (lifechoices=="V" or lifechoices == "v"):
 			print("name address class matg scig eng dug artg")
 			pumuroko=raw_input("Which one?")
-			amirevaltunk=raw_input("What to set it?")
+			amirevaltunk=raw_input("Please enter the new value.")
 			editkegy(sortoedit,pumuroko,amirevaltunk)
 		elif (lifechoices=="R" or lifechoices == "r"):
-			print("Ezt persze nem tudtad volna korabban atgondolni ugye?")
+			print("Let's enter all the new values, shall we?")
 			push=()
 			push+=(raw_input("Name: "),)
 			push+=(raw_input("Address: "),)
@@ -61,8 +60,8 @@ while True:
 			push+=(str(sortoedit),)
 			editke(push)
 	elif (sw=="S" or sw=="s"):
-		print("Faszom fog neked itt keresgetni.")
 		suspect = raw_input("Who you lookin for?")
+		#Here we could add a function that slices the names, so we can look for Bob and not only Bob Smith Jr.
 		c.execute("SELECT * FROM students WHERE name= '%s'" %(suspect))
 		print(c.fetchone())
 		c.execute("SELECT matg, scig, eng, dug, artg FROM students WHERE name= '%s'" %(suspect))
@@ -70,35 +69,12 @@ while True:
 		atlag=0
 		for s in osszesjegyek:
 			atlag+=int(s)
-		
-		#print(atlag)
 		print("Average: ",atlag/5.0)
-		#print("fa")
-		#print(osszesjegyek[0])
-		#for k in xrange(5):
-		#	print(c.fetchone()[k])
 	elif (sw=="P" or sw=="p"):
-		print("Mit kutakodsz te spion geci?")
+		print("Here are all the entries in our database:")
 		c.execute('SELECT * FROM students')
 		print(c.fetchone())
 		dolgok = c.fetchall()
 		for i in dolgok:
 			print(i)
-		
-
-#c.execute('''CREATE TABLE students(id text primary key, name text, address text, class integer, matg text, scig text, eng text, dug text, artg text)''')
-#name #address #class #matg #scig #eng #dug #artg
-
-#Ez meno cucc, keep it
-#for row in c.execute("select name, address from students WHERE rowid=1"):
-#    print(row)
-
-#c.execute("SELECT * FROM students WHERE name=?", "Bob")
-#print(c.fetchone())
-#newEntry("Berci")
-#delete(1)
-
-
-
-
 conn.close()
